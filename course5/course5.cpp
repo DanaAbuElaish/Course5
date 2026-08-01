14f611564d61c9013776da89b21c331138860bcb
Dana Khaled Abu El-aish <45danakhaled123@gmail.com>
2026-08-01 20:01:37 +0300

    Problem 2 ---> Print All Prime Numbers From 1 To N

--- course5/course5.cpp
+++ course5/course5.cpp
@@ -50,3 +50,80 @@ int main() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ****************************************************************** 2 ***************************************************************************
+// ***************************************************** Print All Prime Numbers From 1 To N ******************************************************
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+
+enum enPrimeNotPrime{Prime = 1 , NotPrime = 2};
+
+
+int funcReadPositiveNumber(string message) {
+
+	int number = 0;
+	do {
+		cout << message;
+		cin >> number;
+
+	} while (number < 0);
+
+	return number;
+}
+
+
+enPrimeNotPrime funcCheckPrimeNumber(int number) {
+	int halfNumber = round(number / 2);
+
+	for (int i = 2; i <= halfNumber; i++) {
+		if (number % i == 0)
+			return enPrimeNotPrime::NotPrime;
+	}
+	return enPrimeNotPrime::Prime;
+}
+
+
+
+void funcPrintAllPrimeNumbers(int number) {
+
+	cout << "Prime Numbers are From 1 To " << number << " : \n";
+
+	for (int i = 1; i <= number; i++) {
+
+	if (funcCheckPrimeNumber(i) == enPrimeNotPrime::Prime) {
+
+		cout << i << endl;
+		}
+	}
+}
+
+
+int main() {
+
+	funcPrintAllPrimeNumbers(funcReadPositiveNumber("Please Enter a Positive Number : "));
+
+}
+
+
